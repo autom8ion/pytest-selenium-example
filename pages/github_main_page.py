818196b2a84dfad1ab632a2674998fe3fa8636b6
Kyle Tice <kyletice@rocketmail.com>
2022-09-16 16:34:23 -0600

    Update github_main_page.py

--- pages/github_main_page.py
+++ pages/github_main_page.py
@@ -3,7 +3,7 @@ from pages.BasePage import BasePage
 
 
 class GithubMainPage(BasePage):
-    new_Button = {"by": By.CSS_SELECTOR, "value": "a[class*='btn btn-sm btn-primary'][href*='/new']"}
+    new_Button = {"by": By.CSS_SELECTOR, "value": "div[class*='dashboard-sidebar top'] a[class*='btn btn-sm btn-primary']"}
     find_repository_textfield = {"by": By.CSS_SELECTOR, "value": "input[data-query-name='q']"}
 
     def _init_(self, driver):
